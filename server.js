68e611e9c34e1f370ae4afcecc32ab9bda685035
Aaron <akandle@gmail.com>
2015-06-18 09:08:48 -0700

    End Section Two

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var express = require("express"),
 
 //Route Handlers
 function home (req, res) {
+	console.log("Im not home");
 	res.setHeader("Content-Type", "text/html");
-	res.send('<html><head><title>extraUltima - Online</title></head><body><h1>Welcome to this , er, game.</h1><p><img src="images/sword_and_shield.png"><a href="/adventures">Click me to begin!</a></p></body></html>');
+	res.send('<html><head><title>extraUltima - Online</title></head><body><h1>Welcome to the game.</h1><p><form action="/login" method="POST"><input type="text" name="username"><button type="submit">Start!</button></form></p><p><img src="images/sword_and_shield.png"><a href="/adventures">Click me to begin!</a></p></body></html>');
 }
 
 function adventuresIndex (req, res) {
 	res.setHeader("Content-Type", "text/html");
-	res.send('<html><head><title>Will Ultima Descend?</title></head><body><h1>Are you Ready?</h1><form action="/adventures" method="POST"><button type="submit">Yes, my liege</button></form></body></html>');
+	res.send('<html><head><title>Will Ultima Descend?</title></head><body><h1>Are you Ready, ' + req.session.username + '?</h1><form action="/adventures" method="POST"><button type="submit">Yes, my liege</button></form></body></html>');
 }
 
 function createAdventure (req, res) {
@@ -29,6 +30,30 @@ function showLoot (req, res) {
 	res.send('<html><head><title>Ultima Descends</title></head><body><h1>ID: ' + id + ': Ogre Slayer</h1><p> +9 against Ogres</p></body></html>');
 }
 
+function login(req, res) {
+	if(req.body.username) {
+		req.session.username = req.body.username;
+		res.redirect("/adventures");
+	} else {
+		res.redirect("/");
+	}
+}
+
+function notFound(req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	res.send(404, "<p>Not Found</p>");
+}
+
+function catchErrors (err, req, res, next) {
+	console.log("There was an error");
+	next(err);
+}
+
+function showErrorPage (err, req, res, next) {
+	res.setHeader('Content-Type', 'text/html');
+	res.send(500, "Error man, error");
+}
+
 //Middlewares
 function stoopidLogger(options) {
 	return function stoopidLoggerInner(req, res, next) {
@@ -37,16 +62,36 @@ function stoopidLogger(options) {
 	};
 }
 
+function ensureAuthenticated(req, res, next) {
+	console.log(req.session.username);
+	if (req.session.username) {
+		next();
+	} else {
+		res.redirect('/');
+	}
+}
+
+function readFromDB( req, res, next) {
+	next(new Error("No!!!!!!"));
+}
+
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.cookieSession({secret: "Its a secret", key: "session"}));
 app.use("/", stoopidLogger());
 app.use(app.router);
 app.use(express.static(__dirname + '/public/'));
-
+app.use(notFound);
+app.use(catchErrors);
+app.use(showErrorPage);
 
 //Routes
 app.get('/', home);
-app.get('/adventures', adventuresIndex);
+app.get('/read_from_db', readFromDB);
+app.post('/login', login);
+app.get('/*', ensureAuthenticated);
+app.get('/adventures', ensureAuthenticated, adventuresIndex);
 app.post('/adventures', createAdventure);
 app.put('/adventures/:id', updateAdventure);
 app.get('/loot/:id', showLoot);
