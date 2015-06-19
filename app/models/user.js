8d98a22b93229f0fbc565404a6a895b9f6a14b29
Aaron <akandle@gmail.com>
2015-06-19 16:42:53 -0700

    Email input and validation wired up to mongo.

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,8 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	validate = require('mongoose-validate');
 
 var schema = mongoose.Schema({
-	email: {type: String, unique: true}
+	email: {type: String, required: true, unique: true, validate: [validate.email, 'is not a valid email address']}
 });
 
 schema.set('autoIndex', App.env !== 'production');
