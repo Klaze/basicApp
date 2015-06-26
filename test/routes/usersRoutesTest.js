2d77fdce80ecfe42dfc113455d667babeca5483f
Aaron <akandle@gmail.com>
2015-06-25 17:09:54 -0700

    some more comprehensive route testing

--- test/routes/usersRoutesTest.js
+++ test/routes/usersRoutesTest.js
@@ -1,8 +1,68 @@
 require('../testHelper');
 
-var User = App.model('user');
+var User = App.model('user'),
+	supertest = require('supertest');
 
 describe(__filename, function() {
+	var Test;
+
+	beforeEach(function() {
+		Test = {};
+	});
+
+	describe('Having a user in the database', function() {
+		beforeEach(function(done) {
+			Test.userData = {
+				email: 'existy@example.com',
+				password: 'override'
+			};
+
+			Test.user = new User({email: Test.userData.email, passwordHash: Test.userData.password});
+			Test.user.save(function (err) {
+				assert.ifError(err);
+
+				done();
+			});
+		});
+
+		it('should create a new user when we pass valid data', function(done) {
+			supertest(App.app)
+				.post('/sign_up')
+				.send({email: 'noexisty@example.com', password:'override'})
+				.expect(200)
+				.end(function(err, res) {
+					if (err) {
+						console.log('Error: ', err);
+						console.log(res.body);
+						assert.ifError(err);
+					} else {
+						User.find({email:'noexisty@example.com'}, function(err, users) {
+							assert.ifError(err);
+
+							assert(users[0]);
+							assert.equal(users[0].email, 'noexisty@example.com');
+							done();
+						});
+					}
+				});
+		});
+
+		it('should not create a duplicate user when we pass duplicate data', function(done) {
+			supertest(App.app)
+				.post('/sign_up')
+				.send({email: Test.userData.email, password:'override'})
+				.expect(422)
+				.end(function(err, res) {
+					if (err) {
+						console.log('Error: ', err);
+						console.log(res.body);
+						assert.ifError(err);
+					} else {
+						done();
+					}
+				});
+		});
+	});
 	it('should create the new users', function(done) {
 		var req = {
 			body: {
@@ -28,8 +88,8 @@ describe(__filename, function() {
 						assert.ifError(err);
 						assert.equal(records[0].email, 'test@example.com');
 						done();
-					})
-				})
+					});
+				});
 			}
 		};
 
